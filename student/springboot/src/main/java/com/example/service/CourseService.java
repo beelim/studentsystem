19d9e4227784e4641f54ced08a7a4e6f985e3ba3
cyzyxy <17861062387@163.com>
2024-01-03 20:02:46 +0800

    commit course1

---
+++ student/springboot/src/main/java/com/example/service/CourseService.java
@@ -0,0 +1,25 @@
+package com.example.service;
+
+import com.example.entity.Course;
+import com.example.mapper.CourseMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CourseService
+{
+    @Resource
+    private CourseMapper courseMapper;
+
+    //total 是查询的总数 list是数据列表
+    //pageNum 当前的页码 pageSize是每页的页数
+    public PageInfo<Course> selectPage(Integer pageNum,Integer pageSize){
+        PageHelper.startPage(pageNum,pageSize);
+        List<Course> courseList = courseMapper.selectAll();
+        return PageInfo.of(courseList);
+    }
+}
