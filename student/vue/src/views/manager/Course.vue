d66b20e0e9cb6f1fd2e49c76bb10bcf42e13c6b7
drama <1843331729@qq.com>
2024-01-04 09:00:03 +0800

    new course.vue

--- student/vue/src/views/manager/Course.vue
+++ student/vue/src/views/manager/Course.vue
@@ -2,8 +2,10 @@
   <div>
 
     <div class="card" style="margin-bottom: 10px">
-      <el-input style="width: 260px"; v-model="data.name"  placeholder="请输入课程名称查询":prefix-icon="Search"/>
-      <el-button type="primary" style="margin: 0 10px" >查询</el-button>
+      <el-input style="width: 260px; margin-right: 10px" v-model="data.name" placeholder="请输入课程名称查询" :prefix-icon="Search"/>
+      <el-input style="width: 260px; margin-right: 10px" v-model="data.no" placeholder="请输入课程编号查询" :prefix-icon="Search"/>
+      <el-input style="width: 260px" v-model="data.teacher" placeholder="请输入任课老师查询" :prefix-icon="Search"/>
+      <el-button type="primary" style="margin: 0 10px" @click="load">查询</el-button>
       <el-button type="info" >重置</el-button>
     </div>
 
@@ -17,7 +19,7 @@
         <el-table-column label="名称" prop="name"/>
         <el-table-column label="描述" prop="descr"/>
         <el-table-column label="课时" prop="times"/>
-        <el-table-column label="教师" prop="teacher"/>
+        <el-table-column label="上课教师" prop="teacher"/>
         <el-table-column>
           <template #default="scope">
             <el-button type="primary" plain="false">编辑</el-button>
@@ -26,9 +28,23 @@
         </el-table-column>
       </el-table>
     </div>
-<div class="card">
-    <el-pagination background layout="prev, pager, next" :total="1000" />
-</div>
+    <div class="card">
+      <el-pagination v-model:current-page="data.pageNum"
+                     v-model:page-size="data.pageSize"
+                     background layout="prev, pager, next" :total="1000"/>
+    </div>
+    <div class="card">
+      <el-pagination v-model:current-page="data.pageNum" v-model:page-size="data.pageSize"
+                     @current-change="handleCurrentChange"
+                     background layout="prev,pager,next":total="data.total"/>
+    </div>
+    <el-dialog v-model="data.formVisible" title="课程信息">
+      <el-form :model="data.form">
+        <el-form-item label="Promotion name" :label-width="formLabelWidth">
+          <el-input v-model="data.form.name" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+    </el-dialog>
 <!--    <div class="card">
       <el-pagination background layout="prev, pager, next" :page-size="8" v-model:current-page="data.pageNum" :total="1000"/>
     </div>
@@ -69,8 +85,27 @@ const data = reactive({
     {id:3,name: '大学物理',no:'CS003', descr: '大学物理真有趣', times: '24课时',teacher:'王五'},
     {id:4,name: '数据结构',no:'CS004', descr: '数据结构真有趣', times: '24课时',teacher:'赵六'},
     {id:5,name: '操作系统',no:'CS005', descr: '操作系统真有趣', times: '24课时',teacher:'孙七'},
-  ]
+  ],
+  total:0,
+  pageNum:1,
+  pageSize:5,
+  formVisible:false,
 })
+
+load()
+
+const handelCurrentChange = () =>
+{
+  //当翻页的时候重新加载数据即可
+  load()
+}
+const reset = () => {
+  data.name = ''
+  data.no = ''
+  data.teacher = ''
+  load()
+}
+
 
 /*const handleAdd = () => {
   data.form = {}
