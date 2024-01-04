7dac0f69a4490c26a2fd49a5fe45234c9e5a29b0
qiqi <2315535487@qq.com>
2024-01-04 09:44:03 +0800

    登录注册完整

--- student/springboot/src/main/java/com/example/mapper/StudentMapper.java
+++ student/springboot/src/main/java/com/example/mapper/StudentMapper.java
@@ -1,6 +1,7 @@
 package com.example.mapper;
 
 import com.example.entity.Student;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 public interface StudentMapper {
@@ -8,4 +9,6 @@ public interface StudentMapper {
     @Select("select * from student where username = #{username}")
     Student selectByUsername(String username);
 
+    @Insert("insert into student(username,password,name,phone,email,sex,birth,avatar,role) values(#{username},#{password},#{name},#{phone},#{email},#{sex},#{birth},#{avatar},#{role})")
+    void insert(Student student);
 }
--- student/springboot/src/main/java/com/example/service/StudentService.java
+++ student/springboot/src/main/java/com/example/service/StudentService.java
@@ -1,6 +1,9 @@
 package com.example.service;
 
+import cn.hutool.core.util.ObjectUtil;
+import com.example.common.RoleEnum;
 import com.example.entity.Account;
+import com.example.entity.Student;
 import com.example.exception.CustomException;
 import com.example.mapper.StudentMapper;
 import org.springframework.stereotype.Service;
@@ -29,4 +32,30 @@ public class StudentService {
         // 登录成功
         return dbStudent;
     }
+
+    public void register(Account account){
+        Student student = new Student();
+        student.setUsername(account.getUsername());  //账号
+        student.setPassword(account.getPassword());  //密码
+        this.add(student);
+    }
+
+    /*
+    *新增
+     */
+    private void add(Student student) {
+
+        Student dbstudent = studentMapper.selectByUsername(student.getUsername());
+        if(dbstudent != null){  //已有同名账号 不允许插入
+            throw new CustomException("账号已存在");
+
+        }
+        if(ObjectUtil.isEmpty(student.getName())) {
+            student.setName(student.getUsername());
+        }
+        student.setRole(RoleEnum.STUDENT.name());
+        studentMapper.insert(student);
+
+    }
+
 }
