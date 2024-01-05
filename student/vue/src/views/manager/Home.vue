116ba0258e955babca3b3cfcfdc1658689150223
Scarlett <3030695074@qq.com>
2024-01-05 10:15:59 +0800

    全部测试完成

--- student/vue/src/views/manager/Home.vue
+++ student/vue/src/views/manager/Home.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
 
-    <div class="card" style="line-height: 30px">
-      <div>欢迎<span style="color:dodgerblue">{{ user.name }}</span> ,祝您今天过得开心！</div>
+    <div class="card" style="line-height: 100%">
+      <div style="font-size: 24px; text-align: center">欢迎<span style="color:dodgerblue">{{ user.name }}</span>登录使用学生成绩管理系统</div>
     </div>
+    <div>
+      <div style="display: flex; align-items: center">
+        <img src="@/assets/imgs/home1.png" style="width: 40%; border-radius: 50%"> <img src="@/assets/imgs/home2.png" style="width: 40%; border-radius: 50%">
+      </div>
+      <div style="display: flex; align-items: center">
+        <img src="@/assets/imgs/home2.png" style="width: 40%; border-radius: 50%"> <img src="@/assets/imgs/home4.png" style="width: 40%; border-radius: 50%">
+      </div>
+    </div>
+
 
   </div>
 </template>
